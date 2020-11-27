28f75ae9e6c0b0597afbf2428859671201f4bc4a
Jonathan Warburton <jonathan.warburton09@gmail.com>
2020-11-27 14:56:50 +0100

    Add file reader in preparation

---
+++ src/main/python/file_reader.py
@@ -0,0 +1,8 @@
+def read(file_name: str, path: str = "src/main/resources") -> list:
+    output = []
+    with open(f"{path}/{file_name}") as f:
+        lines = f.readlines()
+        for line in lines:
+            output.append(line.strip())
+
+    return output
